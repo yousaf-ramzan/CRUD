df309ddc4456937ca9ddc8d1acf653068984d195
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-07-21 18:50:36 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Users from "./Users";
+import CreateUsers from "./CreateUsers";
+import UpdateUsers from "./UpdateUsers";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />}></Route>
+          <Route path="/create" element={<CreateUsers />}></Route>
+          <Route path="/update" element={<UpdateUsers />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/UpdateUsers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateUsers = () => {
+  return <div>UpdateUsers</div>;
+};
+
+export default UpdateUsers;
